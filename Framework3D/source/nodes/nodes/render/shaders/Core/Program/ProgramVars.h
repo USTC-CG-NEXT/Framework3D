4bf67fd1a6972f1f5a3c3ff23e029bc84c05ac06
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-09-25 19:03:14 +0800

    WIP

--- Framework3D/source/nodes/nodes/render/shaders/Core/Program/ProgramVars.h
+++ Framework3D/source/nodes/nodes/render/shaders/Core/Program/ProgramVars.h
@@ -26,50 +26,60 @@
  # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **************************************************************************/
 #pragma once
-#include "RtBindingTable.h"
-#include "Core/Macros.h"
-#include "Core/API/fwd.h"
-#include "Core/API/ParameterBlock.h"
-#include "Core/API/ShaderTable.h"
 #include <memory>
 #include <vector>
 
-namespace Falcor
-{
+#include "Core/API/ParameterBlock.h"
+#include "Core/API/ShaderTable.h"
+#include "Core/API/fwd.h"
+#include "Core/Macros.h"
+#include "RtBindingTable.h"
+
+namespace Falcor {
 class Program;
 class ComputeContext;
 
 /**
  * This class manages a program's reflection and variable assignment.
- * It's a high-level abstraction of variables-related concepts such as CBs, texture and sampler assignments, root-signature, descriptor
- * tables, etc.
+ * It's a high-level abstraction of variables-related concepts such as CBs,
+ * texture and sampler assignments, root-signature, descriptor tables, etc.
  */
-class FALCOR_API ProgramVars : public ParameterBlock
-{
-public:
+class FALCOR_API ProgramVars : public ParameterBlock {
+   public:
     /**
      * Create a new graphics vars object.
      * @param[in] pDevice GPU device.
-     * @param[in] pReflector A program reflection object containing the requested declarations.
+     * @param[in] pReflector A program reflection object containing the
+     * requested declarations.
      * @return A new object, or an exception is thrown if creation failed.
      */
-    static ref<ProgramVars> create(nvrhi::DeviceHandle pDevice, const ref<const ProgramReflection>& pReflector);
+    static ref<ProgramVars> create(
+        nvrhi::DeviceHandle pDevice,
+        const ref<const ProgramReflection>& pReflector);
 
     /**
      * Create a new graphics vars object.
      * @param[in] pDevice GPU device.
-     * @param[in] pProg A program containing the requested declarations. The active version of the program is used.
+     * @param[in] pProg A program containing the requested declarations. The
+     * active version of the program is used.
      * @return A new object, or an exception is thrown if creation failed.
      */
-    static ref<ProgramVars> create(nvrhi::DeviceHandle pDevice, const Program* pProg);
+    static ref<ProgramVars> create(
+        nvrhi::DeviceHandle pDevice,
+        const Program* pProg);
 
     /**
      * Get the program reflection interface
      */
-    const ref<const ProgramReflection>& getReflection() const { return mpReflector; }
+    const ref<const ProgramReflection>& getReflection() const
+    {
+        return mpReflector;
+    }
 
-protected:
-    ProgramVars(nvrhi::DeviceHandle pDevice, const ref<const ProgramReflection>& pReflector);
+   protected:
+    ProgramVars(
+        nvrhi::DeviceHandle pDevice,
+        const ref<const ProgramReflection>& pReflector);
 
     ref<const ProgramReflection> mpReflector;
 };
@@ -79,9 +89,8 @@ class RtStateObject;
 /**
  * This class manages a raytracing program's reflection and variable assignment.
  */
-class FALCOR_API RtProgramVars : public ProgramVars
-{
-public:
+class FALCOR_API RtProgramVars : public ProgramVars {
+   public:
     /**
      * Create a new ray tracing vars object.
      * @param[in] pDevice GPU device.
@@ -89,40 +98,69 @@ public:
      * @param[in] pBindingTable The raytracing binding table.
      * @return A new object, or an exception is thrown if creation failed.
      */
-    static ref<RtProgramVars> create(nvrhi::DeviceHandle pDevice, const ref<Program>& pProgram, const ref<RtBindingTable>& pBindingTable);
+    static ref<RtProgramVars> create(
+        nvrhi::DeviceHandle pDevice,
+        const ref<Program>& pProgram,
+        const ref<RtBindingTable>& pBindingTable);
 
     bool prepareShaderTable(RenderContext* pCtx, RtStateObject* pRtso);
 
-    ShaderTablePtr getShaderTable() const { return mpShaderTable; }
-    uint32_t getMissVarsCount() const { return uint32_t(mMissVars.size()); }
-    uint32_t getTotalHitVarsCount() const { return uint32_t(mHitVars.size()); }
-    uint32_t getRayTypeCount() const { return mRayTypeCount; }
-    uint32_t getGeometryCount() const { return mGeometryCount; }
-
-    const std::vector<int32_t>& getUniqueEntryPointGroupIndices() const { return mUniqueEntryPointGroupIndices; }
-
-private:
-    struct EntryPointGroupInfo
+    ShaderTablePtr getShaderTable() const
     {
+        return mpShaderTable;
+    }
+    uint32_t getMissVarsCount() const
+    {
+        return uint32_t(mMissVars.size());
+    }
+    uint32_t getTotalHitVarsCount() const
+    {
+        return uint32_t(mHitVars.size());
+    }
+    uint32_t getRayTypeCount() const
+    {
+        return mRayTypeCount;
+    }
+    uint32_t getGeometryCount() const
+    {
+        return mGeometryCount;
+    }
+
+    const std::vector<int32_t>& getUniqueEntryPointGroupIndices() const
+    {
+        return mUniqueEntryPointGroupIndices;
+    }
+
+   private:
+    struct EntryPointGroupInfo {
         int32_t entryPointGroupIndex = -1;
     };
 
     using VarsVector = std::vector<EntryPointGroupInfo>;
 
-    RtProgramVars(nvrhi::DeviceHandle pDevice, const ref<Program>& pProgram, const ref<RtBindingTable>& pBindingTable);
+    RtProgramVars(
+        nvrhi::DeviceHandle pDevice,
+        const ref<Program>& pProgram,
+        const ref<RtBindingTable>& pBindingTable);
 
     void init(const ref<RtBindingTable>& pBindingTable);
 
-    uint32_t mRayTypeCount = 0;                         ///< Number of ray types (= number of hit groups per geometry).
-    uint32_t mGeometryCount = 0;                        ///< Number of geometries.
-    std::vector<int32_t> mUniqueEntryPointGroupIndices; ///< Indices of all unique entry point groups that we use in the associated program.
+    uint32_t mRayTypeCount =
+        0;  ///< Number of ray types (= number of hit groups per geometry).
+    uint32_t mGeometryCount = 0;  ///< Number of geometries.
+    std::vector<int32_t>
+        mUniqueEntryPointGroupIndices;  ///< Indices of all unique entry point
+                                        ///< groups that we use in the
+                                        ///< associated program.
 
-    mutable ShaderTablePtr mpShaderTable;                    ///< GPU shader table.
-    mutable RtStateObject* mpCurrentRtStateObject = nullptr; ///< The RtStateObject used to create the current shader table.
+    mutable ShaderTablePtr mpShaderTable;  ///< GPU shader table.
+    mutable RtStateObject* mpCurrentRtStateObject =
+        nullptr;  ///< The RtStateObject used to create the current shader
+                  ///< table.
 
     VarsVector mRayGenVars;
     VarsVector mMissVars;
     VarsVector mHitVars;
 };
 
-} // namespace Falcor
+}  // namespace Falcor
