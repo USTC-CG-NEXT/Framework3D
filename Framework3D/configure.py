bf9741df5dd3eb99a851fdc4aea162963b0d26dc
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-10-29 19:06:50 +0800

    Update

--- Framework3D/configure.py
+++ Framework3D/configure.py
@@ -1,219 +1,178 @@
 import zipfile
 import shutil
 import os
+import requests
+from tqdm import tqdm
+import argparse
 
 
-# def copytree_to_binaries(folder, dst=None, target="Debug"):
-#     root_dir = os.getcwd()
-#     print(
-#         "Copying {0} to the binary directory for target {1}...".format(folder, target)
-#     )
-#     if dst is None:
-#         shutil.copytree(
-#             root_dir + "/SDK/" + target + "/" + folder,
-#             root_dir + "/Binaries/" + target + "/",
-#             dirs_exist_ok=True,
-#         )
-#     else:
-#         shutil.copytree(
-#             root_dir + "/SDK/" + target + "/" + folder,
-#             root_dir + "/Binaries/" + target + "/" + dst,
-#             dirs_exist_ok=True,
-#         )
-#     return
-
-
-def copytree_common_to_binaries(folder, target="Debug", dst=None):
+def copytree_common_to_binaries(folder, target="Debug", dst=None, dry_run=False):
     root_dir = os.getcwd()
-    print(
-        "Copying {0} to the binary directory for target {1}...".format(folder, target)
-    )
-    if dst is None:
-        shutil.copytree(
-            root_dir + "/SDK/" + "/" + folder,
-            root_dir + "/Binaries/" + target + "/",
-            dirs_exist_ok=True,
-        )
+    dst_path = os.path.join(root_dir, "Binaries", target, dst or "")
+    if dry_run:
+        print(f"[DRY RUN] Would copy {folder} to {dst_path}")
     else:
         shutil.copytree(
-            root_dir + "/SDK/" + "/" + folder,
-            root_dir + "/Binaries/" + target + "/" + dst,
-            dirs_exist_ok=True,
+            os.path.join(root_dir, "SDK", folder), dst_path, dirs_exist_ok=True
         )
-    return
+        print(f"Copied {folder} to {dst_path}")
 
 
-# def fix_usd_path(target):
-#     if os.name == "posix":
-#         # macOS: no python folder
-#         return
-#     root_dir = os.getcwd()
-#     binary_dir = root_dir + "/Binaries/" + target + "/"
-#     if os.path.exists(binary_dir + "pxr"):
-#         shutil.rmtree(binary_dir + "pxr")
-#     shutil.move(binary_dir + "python/pxr", binary_dir + "pxr")
-#     shutil.rmtree(binary_dir + "python")
+def download_with_progress(url, zip_path, dry_run=False):
+    if dry_run:
+        print(f"[DRY RUN] Would download from {url} to {zip_path}")
+        return
+
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(zip_path), exist_ok=True)
+
+    response = requests.get(url, stream=True)
+    file_size = int(response.headers.get("Content-Length", 0))
+    with tqdm(total=file_size, unit="B", unit_scale=True, desc=zip_path) as pbar:
+        with open(zip_path, "wb") as file_handle:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    file_handle.write(chunk)
+                    pbar.update(len(chunk))
 
 
-# def check_is_script(filename):
-#     if os.name == "nt":
-#         return filename.endswith("cmd") and filename.startswith("usd")
-#     else:
-#         return filename.startswith("usd") and not filename.endswith("so")
+def download_and_extract(url, extract_path, folder, targets, dry_run=False):
+    zip_path = "SDK/cache/" + url.split("/")[-1]
+    if os.path.exists(zip_path):
+        print(f"Using cached file {zip_path}")
+    else:
+        if not dry_run:
+            print(f"Downloading from {url}...")
+        download_with_progress(url, zip_path, dry_run)
 
+    if dry_run:
+        print(f"[DRY RUN] Would extract {zip_path} to {extract_path}")
+        return
 
-# def is_binary(file_name):
-#     try:
-#         with open(file_name, "tr") as check_file:
-#             check_file.read()
-#             return False
-#     except:
-#         return True
-
-
-# def platform_path_name():
-#     if os.name == "nt":
-#         return "path"
-#     else:
-#         return "PATH"
-
-
-# def get_platform_splitter():
-#     if os.name == "nt":
-#         return ";"
-#     else:
-#         return ":"
-
-
-# def fix_usd_python_code(target):
-#     root_dir = os.getcwd()
-#     binary_dir = root_dir + "/Binaries/" + target + "/"
-#     for filename in os.listdir(binary_dir):
-#         if check_is_script(filename):
-#             python_script_name = filename.split(".")[0]
-#             if not is_binary(binary_dir + python_script_name):
-#                 with open(binary_dir + python_script_name, "r") as file:
-#                     filedata = file.read()
-#                 filedata = filedata.replace(
-#                     "from __future__ import print_function\n",
-#                     "from __future__ import print_function\nimport os\nos.environ['{0}'] += '{1}' + os.getcwd()\n".format(
-#                         platform_path_name(), get_platform_splitter()
-#                     ),
-#                 )
-
-#                 # Allow the script to run multiple times
-#                 filedata = filedata.replace(
-#                     "import os\nos.environ['{0}'] += '{1}' + os.getcwd()\nimport os\nos.environ['{0}'] += '{1}' + os.getcwd()\n".format(
-#                         platform_path_name(), get_platform_splitter()
-#                     ),
-#                     "import os\nos.environ['{0}'] += '{1}' + os.getcwd()\n".format(
-#                         platform_path_name(), get_platform_splitter()
-#                     ),
-#                 )
-#                 # Write the file out again
-#                 with open(binary_dir + python_script_name, "w") as file:
-#                     file.write(filedata)
-
-
-# def process_SDK(target):
-#     os.makedirs("./Binaries/" + target + "/", exist_ok=True)
-#     if os.name == "nt":
-#         tbb_lib = "tbb/bin"
-#         folders_list = [
-#             "OpenUSD/bin",
-#             "OpenUSD/bin",
-#             "OpenUSD/lib",
-#             tbb_lib,
-#             "MaterialX/bin",
-#             "openvdb/bin",
-#             "c-blosc/bin",
-#             "zlib/bin",
-#             "Imath/bin",
-#         ]
-#     else:
-#         tbb_lib = "tbb/lib"  # TODO: fix
-#         folders_list = [
-#             "OpenUSD/bin",
-#             "OpenUSD/lib",
-#             "OpenUSD/lib",
-#             tbb_lib,
-#             "MaterialX/lib",
-#             "openvdb/lib",
-#             "c-blosc/lib",
-#             "zlib/lib",
-#             "Imath/lib",
-#         ]
-
-#     if os.name == "nt":
-#         folders_list.append("boost/lib64-msvc-14.3")
-#     for folder in folders_list:
-#         copytree_to_binaries(folder, target=target)
-#     copytree_to_binaries("OpenUSD/plugin/usd", "usd", target=target)
-
-#     copytree_common_to_binaries("embree/bin", target=target)
-#     copytree_common_to_binaries("dxc/bin/x64", target=target)
-#     copytree_common_to_binaries("slang/prelude", dst="slang/prelude", target=target)
-
-#     copytree_to_binaries("libtorch/lib", target=target)
-
-#     if os.name == "nt":
-#         copytree_common_to_binaries("slang/bin/windows-x64/release", target=target)
-#     else:
-#         copytree_common_to_binaries("slang/bin/macosx-aarch64/release", target=target)
-
-#     fix_usd_path(target=target)
-#     if os.name == "nt":
-#         fix_usd_python_code(target=target)
-
-
-# if __name__ == "__main__":
-#     process_SDK("Debug")
-#     process_SDK("Release")
-#     process_SDK("RelWithDebInfo")
-
-import urllib.request
-
-targets = ["Debug", "Release", "RelWithDebInfo"]
-
-
-def download_and_extract_slang():
-    if os.name == "nt":
-        url = "https://github.com/shader-slang/slang/releases/download/v2024.14.3/slang-2024.14.3-windows-x86_64.zip"
-        zip_path = "slang-2024.14.3-windows-x86_64.zip"
-
-    extract_path = "./SDK/slang"
-
-    print("Downloading Slang from {0}...".format(url))
-    urllib.request.urlretrieve(url, zip_path)
-    print("Extracting Slang to {0}...".format(extract_path))
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(extract_path)
-    os.remove(zip_path)
-    print("Slang downloaded and extracted successfully.")
-    if os.name == "nt":
+    print(f"Extracting to {extract_path}...")
+    try:
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(extract_path)
+        print(f"Downloaded and extracted successfully.")
         for target in targets:
-            copytree_common_to_binaries("slang/bin", target=target)
+            copytree_common_to_binaries(folder, target=target, dry_run=dry_run)
+    except Exception as e:
+        print(f"Error extracting {zip_path}: {e}")
 
 
-def download_and_extract_dxc():
-    if os.name == "nt":
-        url = "https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2407/dxc_2024_07_31.zip"
-        zip_path = "dxc_2024_07_31.zip"
+def process_usd(targets, dry_run=False, keep_original_files=True):
+    # First download and extract the source files
+    url = (
+        "https://github.com/PixarAnimationStudios/OpenUSD/archive/refs/tags/v24.11.zip"
+    )
 
-    extract_path = "./SDK/dxc"
+    zip_path = os.path.join(
+        os.path.dirname(__file__), "SDK", "cache", url.split("/")[-1]
+    )
+    if os.path.exists(zip_path):
+        print(f"Using cached file {zip_path}")
+    else:
+        if not dry_run:
+            print(f"Downloading from {url}...")
+        download_with_progress(url, zip_path, dry_run)
 
-    print("Downloading DXC from {0}...".format(url))
-    urllib.request.urlretrieve(url, zip_path)
-    print("Extracting DXC to {0}...".format(extract_path))
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(extract_path)
-    os.remove(zip_path)
-    print("DXC downloaded and extracted successfully.")
-    if os.name == "nt":
-        for target in targets:
-            copytree_common_to_binaries("dxc/bin/x64", target=target)
+    # Extract the downloaded zip file
+    extract_path = os.path.join(os.path.dirname(__file__), "SDK", "OpenUSD", "source")
+    if keep_original_files and os.path.exists(extract_path):
+        print(f"Keeping original files in {extract_path}")
+    else:
+        if dry_run:
+            print(f"[DRY RUN] Would extract {zip_path} to {extract_path}")
+        else:
+            try:
+                with zipfile.ZipFile(zip_path, "r") as zip_ref:
+                    zip_ref.extractall(extract_path)
+                print(f"Downloaded and extracted successfully.")
+            except Exception as e:
+                print(f"Error extracting {zip_path}: {e}")
+                return
+
+    # Call the build script with the specified options
+    build_script = os.path.join(
+        extract_path, "OpenUSD-24.11", "build_scripts", "build_usd.py"
+    )
+
+    # Check if the user has a debug python installed
+    try:
+        os.system("python_d --version")
+        has_python_d = True
+    except:
+        has_python_d = False
+
+    if has_python_d:
+        use_debug_python = "--debug-python "
+    else:
+        use_debug_python = ""
+
+    for target in targets:
+        build_command = f"python {build_script} --openvdb {use_debug_python}--ptex --openimageio --generator Ninja --opencolorio --no-examples --no-tutorials --build-variant {target.lower()} ./SDK/OpenUSD/{target}"
+        if dry_run:
+            print(f"[DRY RUN] Would run: {build_command}")
+        else:
+            os.system(build_command)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Download and configure libraries.")
+    parser.add_argument(
+        "--build_variant", nargs="*", default=["Debug"], help="Specify build variants."
+    )
+    parser.add_argument(
+        "--library",
+        choices=["slang", "dxc", "openusd"],
+        help="Specify the library to configure.",
+    )
+    parser.add_argument("--all", action="store_true", help="Configure all libraries.")
+    parser.add_argument(
+        "--dry-run",
+        "-n",
+        action="store_true",
+        help="Print actions without executing them.",
+    )
+    parser.add_argument(
+        "--keep-original-files",
+        type=bool,
+        default=True,
+        help="Keep original files if the extract path exists.",
+    )
+    args = parser.parse_args()
+
+    targets = args.build_variant
+    dry_run = args.dry_run
+    keep_original_files = args.keep_original_files
+
+    if args.all:
+        args.library = ["openusd", "slang", "dxc"]
+    elif not args.library:
+        print(
+            "No library specified and --all not set. No libraries will be configured."
+        )
+        return
+    else:
+        args.library = [args.library]
+
+    if dry_run:
+        print(f"[DRY RUN] Selected build variants: {targets}")
+
+    urls = {
+        "slang": "https://github.com/shader-slang/slang/releases/download/v2024.14.3/slang-2024.14.3-windows-x86_64.zip",
+        "dxc": "https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2407/dxc_2024_07_31.zip",
+    }
+    folders = {"slang": "slang/bin", "dxc": "dxc/bin/x64"}
+
+    for lib in args.library:
+        if lib == "openusd":
+            process_usd(targets, dry_run, keep_original_files)
+        else:
+            download_and_extract(
+                urls[lib], f"./SDK/{lib}", folders[lib], targets, dry_run
+            )
 
 
 if __name__ == "__main__":
-    download_and_extract_slang()
-    download_and_extract_dxc()
+    main()
