4bd6c49978cfc820ddeaf00f0f471eabfc99778f
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-11-03 23:50:04 +0800

    Restore some function nodes

---
+++ Framework3D/source/basic_nodes/node_storage.cpp
@@ -0,0 +1,25 @@
+#include "basic_node_base.h"
+
+NODE_DEF_OPEN_SCOPE
+NODE_DECLARATION_FUNCTION(storage_in)
+{
+    b.add_input<std::string>("Name").default_val("Storage");
+    b.add_input<entt::meta_any>("storage");
+}
+
+NODE_EXECUTION_FUNCTION(storage_in)
+{
+}
+
+NODE_DECLARATION_FUNCTION(storage_out)
+{
+    b.add_input<std::string>("Name").default_val("Storage");
+    b.add_output<entt::meta_any>("storage");
+}
+
+NODE_EXECUTION_FUNCTION(storage_out)
+{
+}
+
+NODE_DECLARATION_UI(storage);
+NODE_DEF_CLOSE_SCOPE
