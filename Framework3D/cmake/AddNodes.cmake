df1bf929540d78ab6b724904edfa07502ec67b6a
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-11-05 15:38:11 +0800

    Modify node generation script

--- Framework3D/cmake/AddNodes.cmake
+++ Framework3D/cmake/AddNodes.cmake
@@ -3,7 +3,7 @@ include(CMakeParseArguments)
 function(GEN_NODES_JSON TARGET_NAME)
     set(options)
     set(oneValueArgs OUTPUT_JSON)
-    set(multiValueArgs NODES_DIRS CONVERSIONS_DIRS)
+    set(multiValueArgs NODES_DIRS NODES_FILES CONVERSIONS_DIRS CONVERSIONS_FILES)
     cmake_parse_arguments(GEN_NODES_JSON "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     message("GEN_NODES_JSON at ${GEN_NODES_JSON_OUTPUT_JSON}")
@@ -16,34 +16,60 @@ function(GEN_NODES_JSON TARGET_NAME)
         list(APPEND ABS_NODES_DIRS "${dir}")
     endforeach()
 
+    foreach(file IN LISTS GEN_NODES_JSON_NODES_FILES)
+        if(NOT IS_ABSOLUTE ("${file}"))
+            get_filename_component(file "${file}" ABSOLUTE)
+        endif()
+        list(APPEND ABS_NODES_FILES "${file}")
+    endforeach()
+
     foreach(dir IN LISTS GEN_NODES_JSON_CONVERSIONS_DIRS)
-        if(NOT IS_ABSOLUTE "${dir}")
+        if(NOT IS_ABSOLUTE ("${dir}"))
             get_filename_component(dir "${dir}" ABSOLUTE)
         endif()
         list(APPEND ABS_CONVERSIONS_DIRS "${dir}")
     endforeach()
 
-    # Convert the list of directories to a semicolon-separated string
+    foreach(file IN LISTS GEN_NODES_JSON_CONVERSIONS_FILES)
+        if(NOT IS_ABSOLUTE ("${file}"))
+            get_filename_component(file "${file}" ABSOLUTE)
+        endif()
+        list(APPEND ABS_CONVERSIONS_FILES "${file}")
+    endforeach()
+
+    # Convert the list of directories and files to a semicolon-separated string
     string(REPLACE ";" " " NODES_DIRS_STR "${ABS_NODES_DIRS}")
+    string(REPLACE ";" " " NODES_FILES_STR "${ABS_NODES_FILES}")
     string(REPLACE ";" " " CONVERSIONS_DIRS_STR "${ABS_CONVERSIONS_DIRS}")
+    string(REPLACE ";" " " CONVERSIONS_FILES_STR "${ABS_CONVERSIONS_FILES}")
 
     # Construct the command to call the Python script
     set(COMMAND_ARGS ${Python3_EXECUTABLE} ${PROJECT_SOURCE_DIR}/source/util_scripts/nodes_json.py)
-    
+
     if(NODES_DIRS_STR)
-        list(APPEND COMMAND_ARGS --nodes ${NODES_DIRS_STR})
+        list(APPEND COMMAND_ARGS --nodes-dir ${NODES_DIRS_STR})
     endif()
-    
+
+    if(NODES_FILES_STR)
+        list(APPEND COMMAND_ARGS --nodes-files ${NODES_FILES_STR})
+    endif()
+
     if(CONVERSIONS_DIRS_STR)
-        list(APPEND COMMAND_ARGS --conversions ${CONVERSIONS_DIRS_STR})
+        list(APPEND COMMAND_ARGS --conversions-dir ${CONVERSIONS_DIRS_STR})
     endif()
-    
+
+    if(CONVERSIONS_FILES_STR)
+        list(APPEND COMMAND_ARGS --conversions-files ${CONVERSIONS_FILES_STR})
+    endif()
+
     list(APPEND COMMAND_ARGS --output ${GEN_NODES_JSON_OUTPUT_JSON})
+
+    message("COMMAND_ARGS: ${COMMAND_ARGS}")
 
     add_custom_command(
         OUTPUT ${GEN_NODES_JSON_OUTPUT_JSON}
         COMMAND ${COMMAND_ARGS}
-        DEPENDS ${ABS_NODES_DIRS} ${ABS_CONVERSIONS_DIRS}
+        DEPENDS ${ABS_NODES_DIRS} ${ABS_NODES_FILES} ${ABS_CONVERSIONS_DIRS} ${ABS_CONVERSIONS_FILES}
         COMMENT "Generating JSON file with node and conversion information"
     )
 
@@ -55,34 +81,30 @@ function(GEN_NODES_JSON TARGET_NAME)
 endfunction()
 
 function(add_nodes)
-    cmake_parse_arguments(ARG "" "SRC_DIR;TARGET_NAME;CONVERSION_DIR" "SRC_FILES;CONVERSION_FILES;DEP_LIBS;COMPILE_DEFS;COMPILE_OPTIONS" ${ARGN})
+    cmake_parse_arguments(ARG "" "TARGET_NAME" "SRC_DIRS;SRC_FILES;CONVERSION_DIRS;CONVERSION_FILES;DEP_LIBS;COMPILE_DEFS;COMPILE_OPTIONS" ${ARGN})
 
-    if(NOT ARG_SRC_DIR)
-        set(ARG_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR})
-        message("SRC_DIR is not set. Using ${ARG_SRC_DIR}")
+    if(NOT ARG_TARGET_NAME)
+        message(FATAL_ERROR "add_nodes: TARGET_NAME not specified")
     endif()
 
-    if(NOT ARG_SRC_FILES)
-        file(GLOB ARG_SRC_FILES ${ARG_SRC_DIR}/*.cpp)
+    if(NOT ARG_SRC_DIRS)
+        set(ARG_SRC_DIRS ${CMAKE_CURRENT_SOURCE_DIR})
     endif()
 
-    if(NOT ARG_CONVERSION_DIR)
-        message("CONVERSION_DIR is not set.")
-    endif()
+    foreach(src_dir IN LISTS ARG_SRC_DIRS)
+        file(GLOB src_files ${src_dir}/*.cpp)
+        list(APPEND ARG_SRC_FILES_IN_DIRS ${src_files})
+    endforeach()
 
-    if(NOT ARG_CONVERSION_FILES)
-        file(GLOB ARG_CONVERSION_FILES ${ARG_CONVERSION_DIR}/*.cpp)
-    endif()
+    foreach(conversion_dir IN LISTS ARG_CONVERSION_DIRS)
+        file(GLOB conversion_files ${conversion_dir}/*.cpp)
+        list(APPEND ARG_CONVERSION_IN_DIRS ${conversion_files})
+    endforeach()
 
-    set(SRC_DIRS ${ARG_SRC_DIR})
-    set(CONVERSION_DIRS ${ARG_CONVERSION_DIR})
 
-    foreach(source ${ARG_SRC_FILES})
-        get_filename_component(source_dir ${source} DIRECTORY)
-        list(FIND SRC_DIRS ${source_dir} src_dir_index)
-        if(src_dir_index EQUAL -1)
-            list(APPEND SRC_DIRS ${source_dir})
-        endif()
+    set(ALL_THAT_NEEDS_TO_BE_COMPILED ${ARG_SRC_FILES_IN_DIRS} ${ARG_CONVERSION_IN_DIRS} ${ARG_SRC_FILES} ${ARG_CONVERSION_FILES})
+
+    foreach(source ${ALL_THAT_NEEDS_TO_BE_COMPILED})
         get_filename_component(target_name ${source} NAME_WE)
         add_library(${target_name} SHARED ${source})
         set_target_properties(${target_name} PROPERTIES ${OUTPUT_DIR})
@@ -96,28 +118,12 @@ function(add_nodes)
         list(APPEND all_nodes ${target_name})
     endforeach()
 
-    foreach(source ${ARG_CONVERSION_FILES})
-        get_filename_component(source_dir ${source} DIRECTORY)
-        list(FIND CONVERSION_DIRS ${source_dir} conversion_dir_index)
-        if(conversion_dir_index EQUAL -1)
-            list(APPEND CONVERSION_DIRS ${source_dir})
-        endif()
-        get_filename_component(target_name ${source} NAME_WE)
-        add_library(${target_name} SHARED ${source})
-        set_target_properties(${target_name} PROPERTIES ${OUTPUT_DIR})
-        target_link_libraries(${target_name} PUBLIC nodes_core ${ARG_DEP_LIBS})
-        if(ARG_COMPILE_DEFS)
-            target_compile_definitions(${target_name} PRIVATE ${ARG_COMPILE_DEFS})
-        endif()
-        if(ARG_COMPILE_OPTIONS)
-            target_compile_options(${target_name} PRIVATE ${ARG_COMPILE_OPTIONS})
-        endif()
-        list(APPEND all_conversions ${target_name})
-    endforeach()
 
-    GEN_NODES_JSON(${ARG_TARGET_NAME}_json_target 
-        NODES_DIRS ${SRC_DIRS} 
-        CONVERSIONS_DIRS ${CONVERSION_DIRS}
+    GEN_NODES_JSON(${ARG_TARGET_NAME}_json_target
+        NODES_DIRS ${ARG_SRC_DIRS}
+        NODES_FILES ${ARG_SRC_FILES}
+        CONVERSIONS_DIRS ${ARG_CONVERSION_DIRS}
+        CONVERSIONS_FILES ${ARG_CONVERSION_FILES}
         OUTPUT_JSON ${OUT_BINARY_DIR}/${ARG_TARGET_NAME}.json
     )
 
--- Framework3D/source/util_scripts/nodes_json.py
+++ Framework3D/source/util_scripts/nodes_json.py
@@ -3,13 +3,13 @@ import re
 import json
 import argparse
 
-def scan_cpp_files(directories, pattern):
+def scan_cpp_files(directories, files, pattern):
     compiled_pattern = re.compile(pattern)
     nodes = {}
 
     for directory in directories:
-        for root, _, files in os.walk(directory):
-            for file in files:
+        for root, _, dir_files in os.walk(directory):
+            for file in dir_files:
                 if file.endswith('.cpp'):
                     file_path = os.path.join(root, file)
                     with open(file_path, 'r') as f:
@@ -19,26 +19,37 @@ def scan_cpp_files(directories, pattern):
                             file_name_without_suffix = os.path.splitext(file)[0]
                             nodes[file_name_without_suffix] = matches
 
+    for file in files:
+        if file.endswith('.cpp'):
+            with open(file, 'r') as f:
+                content = f.read()
+                matches = compiled_pattern.findall(content)
+                if matches:
+                    file_name_without_suffix = os.path.splitext(os.path.basename(file))[0]
+                    nodes[file_name_without_suffix] = matches
+
     return nodes
 
 def main():
     parser = argparse.ArgumentParser(description='Scan cpp files for NODE_EXECUTION_FUNCTION and CONVERSION_EXECUTION_FUNCTION and generate JSON.')
-    parser.add_argument('--nodes', nargs='+', type=str, help='Paths to the directories containing node cpp files', default=[])
-    parser.add_argument('--conversions', nargs='+', type=str, help='Paths to the directories containing conversion cpp files', default=[])
+    parser.add_argument('--nodes-dir', nargs='+', type=str, help='Paths to the directories containing node cpp files', default=[])
+    parser.add_argument('--nodes-files', nargs='+', type=str, help='Paths to the node cpp files', default=[])
+    parser.add_argument('--conversions-dir', nargs='+', type=str, help='Paths to the directories containing conversion cpp files', default=[])
+    parser.add_argument('--conversions-files', nargs='+', type=str, help='Paths to the conversion cpp files', default=[])
     parser.add_argument('--output', type=str, help='Path to the output JSON file')
     args = parser.parse_args()
 
     result = {}
 
-    if args.nodes:
+    if args.nodes_dir or args.nodes_files:
         node_pattern = r'NODE_EXECUTION_FUNCTION\((\w+)\)'
-        result['nodes'] = scan_cpp_files(args.nodes, node_pattern)
+        result['nodes'] = scan_cpp_files(args.nodes_dir, args.nodes_files, node_pattern)
     else:
         result['nodes'] = {}
 
-    if args.conversions:
+    if args.conversions_dir or args.conversions_files:
         conversion_pattern = r'CONVERSION_EXECUTION_FUNCTION\((\w+),\s*(\w+)\)'
-        conversions = scan_cpp_files(args.conversions, conversion_pattern)
+        conversions = scan_cpp_files(args.conversions_dir, args.conversions_files, conversion_pattern)
         result['conversions'] = {k: [f"{match[0]}_to_{match[1]}" for match in v] for k, v in conversions.items()}
     else:
         result['conversions'] = {}
