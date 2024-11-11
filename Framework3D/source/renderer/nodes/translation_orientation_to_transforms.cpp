1bd31fee6350c8c9d9b8a26b2d2cba38952997e1
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-11-11 19:46:45 +0800

    WIP

---
+++ Framework3D/source/renderer/nodes/translation_orientation_to_transforms.cpp
@@ -0,0 +1,16 @@
+
+#include "nodes/core/def/node_def.hpp"
+
+NODE_DEF_OPEN_SCOPE
+NODE_DECLARATION_FUNCTION(translation_orientation_to_transforms)
+{
+    // Function content omitted
+}
+
+NODE_EXECUTION_FUNCTION(translation_orientation_to_transforms)
+{
+    // Function content omitted
+}
+
+NODE_DECLARATION_UI(translation_orientation_to_transforms);
+NODE_DEF_CLOSE_SCOPE
