ddee0120b9582c45be8b4d7ba1b588da850ae478
Jerry-Shen0527 <jerry_shen0527@outlook.com>
2024-12-02 12:55:20 +0800

    WIP

--- Framework3D/source/diff_optics/tests/dO_GUI_T.cpp
+++ Framework3D/source/diff_optics/tests/dO_GUI_T.cpp
@@ -101,53 +101,7 @@ const char* str = R"(
 }
 
 )";
-//
-// TEST(dO_GUI_T, diff_optics_gui)
-//{
-//    Window window;
-//
-//    LensSystem lens_system;
-//    lens_system.add_lens(std::make_shared<NullLayer>(0, 0));
-//    lens_system.add_lens(std::make_shared<Occluder>(1.f, 10, 0));
-//    lens_system.add_lens(std::make_shared<SphericalLens>(10.f, 5.f, 12.f, 0));
-//    lens_system.add_lens(std::make_shared<Occluder>(3.f, 15, 0));
-//
-//    lens_system.add_lens(std::make_shared<Occluder>(0.1f, 20, 0));
-//
-//    lens_system.add_lens(std::make_shared<NullLayer>(30.f, 0));
-//
-//    auto gui = createDiffOpticsGUI(&lens_system);
-//
-//    window.register_widget(std::move(gui));
-//
-//    window.run();
-//}
-//
-// TEST(dO_T, diff_optics)
-//// int main()
-//{
-//    Window window;
-//    LensSystem lens_system;
-//
-//    std::string json = std::string(str);
-//    lens_system.deserialize(json);
-//}
-//
-// TEST(dO_GUI_T, diff_optics_json)
-//{
-//    Window window;
-//
-//    LensSystem lens_system;
-//
-//    std::string json = std::string(str);
-//    lens_system.deserialize(json);
-//
-//    auto gui = createDiffOpticsGUI(&lens_system);
-//
-//    window.register_widget(std::move(gui));
-//
-//    window.run();
-//}
+
 
 TEST(dO_GUI_T, draw_rays)
 {
