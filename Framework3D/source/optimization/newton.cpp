8d5d29e4c6930ecc71ab6b8c55640e8a5de37b90
yusen-wu <2320803722@qq.com>
2024-12-22 00:02:35 +0800

    rebase了一下，并且重新修改了一下newton等使得支持任意数量的自变量

--- Framework3D/source/optimization/newton.cpp
+++ Framework3D/source/optimization/newton.cpp
@@ -1,61 +1,57 @@
 #include <Eigen/Eigen>
-#include "nodes/core/def/node_def.hpp"
 #include <autodiff/reverse/var.hpp>
 #include <autodiff/reverse/var/eigen.hpp>
 
-NODE_DEF_OPEN_SCOPE
+#include "nodes/core/def/node_def.hpp"
 
 using namespace autodiff;
 
+NODE_DEF_OPEN_SCOPE
+
 NODE_DECLARATION_FUNCTION(newton)
 {
-    b.add_input<std::function<double(Eigen::Vector3d)>>("Cost function");
-    b.add_input<Eigen::Vector3d>("Initial point");
+    b.add_input<std::function<float(Eigen::VectorXf)>>("Cost function");
+    b.add_input<Eigen::VectorXf>("Initial point");
     b.add_input<int>("Max iterations");
-    b.add_input<double>("Tolerance");
-    b.add_output<Eigen::Vector3d>("Minimum point");
+    b.add_input<float>("Tolerance");
+    b.add_output<Eigen::VectorXf>("Minimum point");
 }
 
 NODE_EXECUTION_FUNCTION(newton)
 {
-    auto f0 = params.get_input<std::function<double(Eigen::Vector3d)>>(
+    auto f0 = params.get_input<std::function<float(Eigen::VectorXf)>>(
         "Cost function");
 
     auto f = [f0](const ArrayXvar& x) -> var {
-        Eigen::Vector3d x_old;
-        x_old[0] = val(x[0]);
-        x_old[1] = val(x[1]);
-        x_old[2] = val(x[2]);
-        double y = f0(x_old);
+        Eigen::VectorXf x_old = x.template cast<float>();
+        float y = f0(x_old);
         return var(y);
     };
 
-    auto x0 = params.get_input<Eigen::Vector3d>("Initial point");
+    auto x0 = params.get_input<Eigen::VectorXf>("Initial point");
 
-    VectorXvar x(3);
+    VectorXvar x = x0.template cast<var>();
+
     const int max_iterations = params.get_input<int>("Max iterations");
-    const double tolerance = params.get_input<double>("Tolerance");
+    const float tolerance = params.get_input<float>("Tolerance");
 
-    Eigen::Vector3d x_old = x0;
-    Eigen::Vector3d x_new;
-
-    x << x0[0], x0[1], x0[2];
-
+    Eigen::VectorXf x_old = x0;
+    Eigen::VectorXf x_new = x0;
     for (int i = 0; i < max_iterations; ++i) {
         var u = f(x);
-
-        Eigen::Vector3d g;
-        Eigen::Matrix3d H = hessian(u, x, g);
-        Eigen::Vector3d d = H.colPivHouseholderQr().solve(g);
-
+        Eigen::VectorXd g;
+        Eigen::MatrixXd H = hessian(u, x, g);
+        Eigen::VectorXd d0 = H.colPivHouseholderQr().solve(g);
+        Eigen::VectorXf d = d0.cast<float>();
         x_new = x_old - d;
         if (d.norm() < tolerance) {
             break;
         }
         x_old = x_new;
-        x << x_new[0], x_new[1], x_new[2];
+        x = x_new.template cast<var>();
     }
-    params.set_output<Eigen::Vector3d>("Minimum point", std::move(x_new));
+    params.set_output<Eigen::VectorXf>("Minimum point", std::move(x_new));
+    return true;
 }
 
 NODE_DECLARATION_UI(newton);
