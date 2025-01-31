84799682fdd08ce2c5f3eb4df6056c480d0e1c96
Pengfei Shen <jerry_shen0527@outlook.com>
2025-01-31 13:22:08 +0800

    Move trait

--- Framework3D/source/Core/nodes/core/include/nodes/core/node.hpp
+++ Framework3D/source/Core/nodes/core/include/nodes/core/node.hpp
@@ -156,47 +156,7 @@ class BaseSocketDeclarationBuilder {
     friend class NodeDeclarationBuilder;
 };
 
-template<typename T>
-struct ValueTrait {
-    static constexpr bool has_min = false;
-    static constexpr bool has_max = false;
-    static constexpr bool has_default = false;
-};
-
-template<>
-struct ValueTrait<bool> {
-    static constexpr bool has_min = false;
-    static constexpr bool has_max = false;
-    static constexpr bool has_default = true;
-};
-
-template<>
-struct ValueTrait<int> {
-    static constexpr bool has_min = true;
-    static constexpr bool has_max = true;
-    static constexpr bool has_default = true;
-};
-
-template<>
-struct ValueTrait<float> {
-    static constexpr bool has_min = true;
-    static constexpr bool has_max = true;
-    static constexpr bool has_default = true;
-};
-
-template<>
-struct ValueTrait<double> {
-    static constexpr bool has_min = true;
-    static constexpr bool has_max = true;
-    static constexpr bool has_default = true;
-};
-
-template<>
-struct ValueTrait<std::string> {
-    static constexpr bool has_min = false;
-    static constexpr bool has_max = false;
-    static constexpr bool has_default = true;
-};
+#include "socket_trait.inl"
 
 template<
     typename T,
